73ee42db3bd1fe305e9efd40050c7b5845ab6ba9
WillCup <willcup@163.com>
2015-02-05 09:26:22 +0000

    Add mysql consts

--- src/main/java/com/will/Consts.java
+++ src/main/java/com/will/Consts.java
@@ -13,4 +13,9 @@ public class Consts {
 
     public static final String REGEX_NOT_LETTER = "[^A-Za-z0-9]";
     
+    public static final String MYSQL_CONN_PREFIX = "jdbc:mysql://";
+    public static final String LOCAL = "127.0.0.1";
+    public static final String COLON = ":";
+    public static final String MYSQL_CONN_SUFFIX = "/information_schema?&useUnicode=true&characterEncoding=UTF-8";
+    
 }
